54cd2f742aaa622ad7e5c16476b73b06c12e7c40
wzz <97061125@qq.com>
2019-09-30 17:49:42 +0800

    高可用注册

---
+++ src/test/java/com/youdao/wzz/MyEureka2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.youdao.wzz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyEureka2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
